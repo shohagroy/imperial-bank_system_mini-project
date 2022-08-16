aacd46c95392c20d5772a846c93b993a806f7ac3
shohagroy <pkshohag240@gmail.com>
2022-08-16 22:17:28 +0600

    home page html update

--- js/login.js
+++ js/login.js
@@ -1,17 +1,36 @@
 
 
+// login button function 
+function loginprotal(){
 
-// get email and password value
-const emailFild = document.getElementById('email-fild');
-const passwordFild = document.getElementById('password-fild');
+    // get email and password value and set varible
+    const emailFild = document.getElementById('email-fild');
+    const passwordFild = document.getElementById('password-fild');
 
-const emailValue = emailFild.value;
-const passwordValue = passwordFild.value;
+    const email = emailFild.value;
+    const password = passwordFild.value;
 
+    // input rong and right waring
+    if(email === "" || password === ""){
+        alert("Please Enter Your Email and Password !")
+        emailFild.value = "";
+        passwordFild.value = "";
+    }
+    else{
+        
+        if(email == "abc@gmail.com" && password == '1234'){
 
-// loginButton function 
-document.getElementById('login-button');
-loginButton.addEventListener('click', function(){
+            window.location.href = "home.html"
 
-    console.log(emailValue, passwordValue )
-})
+            emailFild.value = "";
+            passwordFild.value = "";
+        }
+        else{
+            alert("Email or Password Wrong!")
+            emailFild.value = "";
+            passwordFild.value = "";
+        }
+    
+    }
+   
+}
