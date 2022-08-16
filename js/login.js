00dddb417653d521b281012218000cd78f1f67f8
unknown <pkshphag240@gmail.com>
2022-08-16 19:52:47 +0600

    js code update

--- js/login.js
+++ js/login.js
@@ -1,4 +1,33 @@
 
+// get email and password value
+const loginButton  = document.getElementById('login-button');
+const emailFild = document.getElementById('email-fild');
+const passwordFild = document.getElementById('password-fild');
 
-// stap: 1
-// login button id get and function strat
+const emailValue = emailFild.value;
+const passwordValue = emailFild.value;
+
+
+
+// button disable
+loginButton.setAttribute("disabled", "true");
+// button enable 
+
+
+if(emailValue == "" && passwordValue == ""){
+    prompt("Please Enter Your Email and Password and Click Login Button")
+}{
+    passwordValue.setAttribute
+}
+
+console.log(emailValue, passwordValue );
+
+
+
+
+
+// login function 
+function loginProtal(){
+    console.log('hello')
+
+}
